f3597e8a69d5c796b74fa9cf230dd14b02722287
dicksonmateen-arch <dicksonmateen@gmail.com>
2026-02-21 20:56:57 +0000

    Add files via upload

---
+++ operator prestidents.py
@@ -0,0 +1,17 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) * x / y
+print("Value of (v+w) * x / y is", z)
+
+
+
+name = "James"
+age = 10
+
+if name == "James" or name == "Jacop" and age >= 2 :
+    print("Hello! Welcome.")
+else :
+    print("See you later!")
